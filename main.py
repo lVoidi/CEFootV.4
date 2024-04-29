9da478b79225a24f0ab167019addd5abecaea267
lvoidi <roarba011@gmail.com>
2024-04-29 16:04:30 -0600

    Algoritmo que espera el disparo del jugador y que detecta si es gol o no

--- main.py
+++ main.py
@@ -14,19 +14,54 @@ red_team = Pin(27, Pin.OUT)
 blue_team.high()
 red_team.low()
 
+
+# Botones de cada paleta 
+
+goal_buttons = [
+    Pin(7, Pin.IN, Pin.PULL_UP),
+    Pin(8, Pin.IN, Pin.PULL_UP),
+    Pin(9, Pin.IN, Pin.PULL_UP),
+    Pin(10, Pin.IN, Pin.PULL_UP),
+    Pin(11, Pin.IN, Pin.PULL_UP),
+    Pin(12, Pin.IN, Pin.PULL_UP),
+]
+
+
+def listen_to_goal(goalkeeper_indices, team):
+    values = [
+        button.value() for button in goal_buttons
+    ]
+    while not any(values):
+        values = [
+            button.value() for button in goal_buttons
+        ]
+    
+    for index, value in values:
+        if value and i in goalkeeper_indices:
+            # Código para el gol de un equipo
+            pass 
+        else:
+            # Código para cuando el equipo no hace gol
+
+
 def main():
-    current_team_playing = blue_team
+    current_team_playing = "blue"
     # Leds de cada paleta
     goal = [i for i in range(6)]
     while True:
         if change_player_signal:
+            current_team_playing = "red"
             blue_team.low()
             red_team.high()
         else:
+            current_team_playing = "blue"
             blue_team.high()
             red_team.low()
 
         anotation_algorithm = random.choice(1, 3)
+
+        # Índices de las paletas en las que está el portero
+        index_list = []
         match anotation_algorithm:
             case 1:
                 goalkeeper_index = random.sample(goal, 2)
@@ -50,6 +85,7 @@ def main():
                     index_list = list(filter(lambda x: (x % 2 == 0), goal))
                 else:
                     index_list = list(filter(lambda x: (x % 2 == 1), goal))
+        listen_to_goal(index_list, current_team_playing)
 
 main_thread = threading.Thread(target=main)
 main_thread.start()
