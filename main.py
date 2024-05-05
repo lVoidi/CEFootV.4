9fc8ea7bb414086b3e479e3ab9612e9713ac10ab
dgcgary <d.garcia.1@estudiantec.cr>
2024-05-05 16:14:32 -0600

    agregado servidor http

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from machine import Pin, ADC#, UART
+from machine import Pin, ADC, UART
+import network
 import random
+import socket
 import time
 import uos 
 
@@ -7,25 +9,29 @@ data_pin = Pin(13, Pin.OUT)
 latch_pin = Pin(15, Pin.OUT)
 clock_pin = Pin(14, Pin.OUT)
 
+
+
 # Boton del team y leds
-change_player_signal = Pin(5, Pin.IN, Pin.PULL_UP)
+change_player_signal = Pin(28, Pin.IN, Pin.PULL_UP)
+current_team_playing = "blue"
+
 blue_team = {
-    "pin": Pin(26, Pin.OUT),
+    "pin": Pin(0, Pin.OUT),
     "nombre": "", 
     "goles": {}, # {"artillero": n} donde n es la cantidad de goles 
     "fallados": [], 
 }
 
 red_team = {
-    "pin": Pin(27, Pin.OUT),
+    "pin": Pin(1, Pin.OUT),
     "nombre": "", 
     "goles": {}, # {"artillero": n} donde n es la cantidad de goles 
     "fallados": [], 
 }
 
 # Empieza con el azul como team inicial
-blue_team["pin"].high()
-red_team["pin"].low()
+#blue_team["pin"].value(1)
+#red_team["pin"].value(0)
 
 
 # Botones de cada paleta 
@@ -38,41 +44,76 @@ goal_buttons = [
     Pin(12, Pin.IN),
 ]
 
-#baudrate = 115200
-#uart = UART(0, baudrate=baudrate) 
-#uart.init(baudrate, bits=8, parity=None, stop=1)
-#uos.dupterm(uart)
+goal_leds = [
+    Pin(18, Pin.OUT),
+    Pin(17, Pin.OUT),
+    Pin(16, Pin.OUT),
+    Pin(3, Pin.OUT),
+    Pin(4, Pin.OUT),
+    Pin(5, Pin.OUT),
+]
+
+baudrate = 115200
+
+
+pot = ADC(26)
+
+def return_pot_val():
+    return int((pot.read_u16()/65535)*100)//50
 
 def listen_to_goal(goalkeeper_indices, team):
+    global blue_team,red_team, current_team_playing
+    pressed_button_index = 0
     values = [
         button.value() for button in goal_buttons
     ]
     while not any(values):
+        print(change_player_signal.value())
+        if change_player_signal.value() and current_team_playing == "blue":
+            print(red_team["pin"].value(), blue_team["pin"].value())
+            current_team_playing = "red"
+            blue_team["pin"].value(0)
+            red_team["pin"].value(1)
+        elif change_player_signal.value() and current_team_playing == "red":
+            current_team_playing = "blue"
+            blue_team["pin"].value(1)
+            red_team["pin"].value(0)
+        
+        
         values = [
             button.value() for button in goal_buttons
         ]
+        time.sleep(0.5)
     
     for index, value in enumerate(values):
+        if value:
+            pressed_button_index = index
         if value and index in goalkeeper_indices:
+            
             print("A"*128)
     else:    
         print("B"*128)
             # Código para cuando el equipo no hace gol
+    goal_leds[pressed_button_index].value(1)
+    print(pressed_button_index, goal_leds[pressed_button_index])
     time.sleep(2)
+    goal_leds[pressed_button_index].value(0)
 
 def main():
+    global blue_team,red_team, current_team_playing
     current_team_playing = "blue"
     # Leds de cada paleta
     goal = [i for i in range(6)]
     while True:
-        if change_player_signal.value():
+        print(change_player_signal.value())
+        if change_player_signal.value() and current_team_playing == "blue":
             current_team_playing = "red"
-            blue_team["pin"].low()
-            red_team["pin"].high()
-        else:
+            blue_team["pin"].value(0)
+            red_team["pin"].value(1)
+        elif change_player_signal.value() and current_team_playing == "red":
             current_team_playing = "blue"
-            blue_team["pin"].high()
-            red_team["pin"].low()
+            blue_team["pin"].value(1)
+            red_team["pin"].value(0)
 
         anotation_algorithm = random.randint(1, 3)
 
@@ -99,6 +140,55 @@ def main():
                 index_list = list(filter(lambda x: (x % 2 == 1), goal))
         listen_to_goal(index_list, current_team_playing)
 
-if __name__ == "__main__":
-    main()
+
+def create_http_server():
+    pins = [Pin(i, Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
+
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP8266 Pins</title> </head>
+        <body> <h1>ESP8266 Pins</h1>
+            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+        response = html % '\n'.join(rows)
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+
+
+if __name__ == "__main__":  
+    do_connect()
+    create_http_server()
+    #main()
     
